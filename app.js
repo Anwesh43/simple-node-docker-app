fcf74d11974a11ba03fb52669c272468bfdaace3
Anwesh43 <anweshthecool0@gmail.com>
2020-02-23 17:47:11 +0530

    Copying code changes to docker

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ app.get('/hello', (req, res) => {
     res.send("hello world")
 })
 
+app.get('/hello/:name/:age', (req, res) => {
+    const {name, age} = req.params
+    res.send(`hello ${name} ${age}`)
+})
+
 app.listen(8080, () => {
     console.log("listening on 8080");
 })
